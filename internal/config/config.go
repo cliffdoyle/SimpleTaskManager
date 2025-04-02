a1912327931f30a6a700c37a1e4c1074d02172b8
clomollo <oyoocliff471@gmail.com>
2025-04-03 01:18:49 +0300

    added helper functions that does the actual loading of the config variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+
+import (
+	"fmt"
+	"os"
+)
+
 //Config holds application configuration
 type Config struct {
 	DBUser string
@@ -9,15 +15,34 @@ type Config struct {
 	DBPort string
 
 }
-
+//Loads configuration fron environment variables
 func LoadConfig()*Config{
 	config := &Config{
         DBUser:     getEnv("DB_USER", "postgres"),
         DBPassword: getEnv("DB_PASSWORD", "postgres"),
         DBName:     getEnv("DB_NAME", "gotasker"),
         DBHost:     getEnv("DB_HOST", "localhost"),
-        Port:       getEnv("PORT", "8080"),
+        DBPort:       getEnv("PORT", "8080"),
     }
 	return config
 
+}
+//returns database connection string
+func (c *Config)GetDSN()string{
+return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	c.DBHost,
+	c.DBPort,
+	c.DBUser,
+	c.DBPassword,
+	c.DBName,
+)
+}
+
+//helper function to get environment variables with a default value
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }
