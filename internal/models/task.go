379787f63d06e32e94917bffc7d7d0dc01f30ee4
clomollo <oyoocliff471@gmail.com>
2025-04-03 01:40:56 +0300

    feat: added models struct that will represent an individual task i the system

---
+++ internal/models/task.go
@@ -0,0 +1,12 @@
+package models
+
+
+type Task struct{
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Status bool `json:"completed"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+
+}
