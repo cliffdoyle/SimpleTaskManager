e3b234d428deea3cdbf72ad622219851c1a8ab97
clomollo <oyoocliff471@gmail.com>
2025-04-03 08:18:02 +0300

    feat:added function that retrieves a single task from the database using the id

--- internal/handlers/task.go
+++ internal/handlers/task.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/cliffdoyle/SimpleTaskManager.git/internal/models"
+	"github.com/gorilla/mux"
 )
 
 
@@ -52,5 +53,33 @@ func (t *TaskHandler)GetTasks(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(tasks)
 
 	
+	http.Error(w, "Not implemented", http.StatusNotImplemented)
+}
+
+//GetTask returns a single task
+func (t *TaskHandler)GetTask(w http.ResponseWriter, r *http.Request){
+	//TODO implement me
+	params:=mux.Vars(r)
+	id:=params["id"]
+
+	//querry task
+	var task models.Task
+
+	err:=t.DB.QueryRow(`SELECT id, title, description,status,created_at,updated_at
+	FROM tasks
+	WHERE id=$1`,id).Scan(&task.ID,&task.Title,&task.Description,&task.Status,&task.CreatedAt,&task.UpdatedAt)
+	if err!=nil{
+		if err==sql.ErrNoRows{
+			http.Error(w, "Task not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//Return Json response
+	w.Header().Set("content-type","application/jsom")
+	json.NewEncoder(w).Encode(task)
+
 	http.Error(w, "Not implemented", http.StatusNotImplemented)
 }
